99cbd28af88af4532e0ca50c90a568337bc805c4
jane-create <82155865+jane-create@users.noreply.github.com>
2021-04-13 09:32:30 +0530

    Add files via upload

---
+++ wordpad.py
@@ -0,0 +1,25 @@
+from tkinter import *
+
+top = Tk()
+top.geometry('500x500')
+top.title('Untitled - Notepad')
+
+
+
+l1 =Label(top, text ='File')
+l2 =Label(top, text ='Edit')
+l3 =Label(top, text ='Format')
+l4 =Label(top, text ='View')
+l5 =Label(top, text = 'Help')
+l1.grid(row =0, column =1)
+l2.grid(row =0, column =2)
+l3.grid(row =0, column =3)
+l4.grid(row =0, column =4)
+l5.grid(row =0, column =5)
+
+
+
+
+
+top.mainloop()
+
